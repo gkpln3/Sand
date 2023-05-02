68d72e1ff3609b56a31e63fdcce7a359aa1efe4b
Guy Kaplan <gkpln3@gmail.com>
2023-05-02 10:13:01 +0300

    Now live on pip :)

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='docker-sand',
-    version='0.0.1',
+    version='0.0.3',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -14,6 +19,8 @@ setup(
     ],
     author='Guy Kaplan',
     description='Sand is a Dockerfile generator based on python that allows you to write your Dockerfile in a more convenient way.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     keywords='sand docker dockerfile build',
     url='https://github.com/gkpln3/Sand',
